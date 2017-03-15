2c71d337a8e96b9f10f1e3b2288d00904898d955
PMinKyung <mkyung14@naver.com>
2017-03-15 13:25:23 +0900

    version1 upload

---
+++ board_logout.php
@@ -0,0 +1,15 @@
+<?
+//board_logout.php : 사이트에서 로그아웃한다.
+// 1. 공통 인클루드 파일
+include ("./include.php");
+
+// 2. 모든 세션값을 빈값으로 
+$_SESSION[user_idx] = "";
+$_SESSION[user_id] = "";
+$_SESSION[user_name] = "";
+
+?>
+<script>
+alert(" 로그아웃이 되어습니다.");
+location.replace("board_login.php");
+</script>
