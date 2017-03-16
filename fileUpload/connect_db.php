120329485e3a4dc059737972762345a5e92cdcca
Seongmin Jeong <death2850@gmail.com>
2017-03-16 16:00:41 +0900

    DB connection v0.1 in 20170316

---
+++ fileUpload/connect_db.php
@@ -0,0 +1,14 @@
+<?php
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pw = "1234";
+
+$conn = mysql_connect($db_host, $db_user, $db_pw) or die('database connection failed!');
+
+// db connect
+//mysql_select_db($db_name, $conn);
+
+
+
+?>
