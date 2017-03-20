0ff9e17bfbc851be963f7dc93d56173fbf8fa8e8
Seongmin Jeong <death2850@gmail.com>
2017-03-20 16:18:40 +0900

    20170320, db connection check & db query practice

--- fileUpload/connect_db.php
+++ fileUpload/connect_db.php
@@ -1,14 +1,53 @@
 <?php
 
-$db_host = "localhost";
-$db_user = "root";
-$db_pw = "1234";
+$db_host = "localhost"; // db가 있는 host
+$db_user = "root"; // db에 접속할 계정 id
+$db_pw = "tjdals12"; // db password
 
-$conn = mysql_connect($db_host, $db_user, $db_pw) or die('database connection failed!');
+$db = mysql_connect($db_host, $db_user, $db_pw, ""); // db login
 
-// db connect
-//mysql_select_db($db_name, $conn);
+if(!$db) die("Connection failed: ". mysql_error()); // db alive check
 
+$query = mysql_query('show databases;'); // query : database list 보기
 
+if(!$query) echo "query failed";
+else {
+	while($instance = mysql_fetch_assoc($query)) {
+		print_r($instance);
+		echo "<br>";
+	}
+}
+
+$dbName = 'sosfinder_sm';
+$tableName = 'tb_upfile_info';
+
+$query = mysql_query('use '.$dbName.';'); // query: 해당 table의 database 사용 선언
+
+$query = mysql_query('desc '.$tableName.';'); // query: 해당 table의 구조 보기
+
+if(!$query) echo "query failed";
+else {
+	while($instance = mysql_fetch_assoc($query)) {
+		foreach($instance as $key => $value) {
+			echo "$key: $value\t";
+		}
+		echo "<br>";
+	}
+}
+
+// $query = mysql_query('select * from '.$tableName.';'); // query: 해당 table의 원소 검색
+
+// if(!$query) echo "query failed";
+// else {
+// 	while($instance = mysql_fetch_assoc($query)) {
+// 		foreach($instance as $key => $value) {
+// 			echo "$key: $value\t";
+// 		}
+// 		echo "<br>";
+// 	}
+// }
+
+mysql_free_result($query);
+mysql_close($db);
 
 ?>
